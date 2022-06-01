8db51bcbc99fd406fcdb67735f159450e66439b2
adisk <adiskd22gm@gmail.com>
2022-06-01 22:38:41 +0600

    feat: added three Variables.

---
+++ 31.05.22/script.js
@@ -0,0 +1,21 @@
+//Первая задача
+let userSalaries = {
+    Ella: 50,
+    Sophia: 70,
+    Ellie: 40,
+}
+alert(userSalaries.Ella);
+alert(userSalaries.Ellie);
+
+//Вторая задача
+let alphabet = {
+    a: 5,
+    b: 6,
+    c: 10,
+}
+alert(alphabet.a + alphabet.b + alphabet.c);
+
+//Третья задача
+let number;
+let result = (data === true) ? number = 3 : number = 5;
+alert(result);
