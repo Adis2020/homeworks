4ca60212a65377d11d41e9eac85a4a020f000aea
adisk <adiskd22gm@gmail.com>
2022-06-05 23:24:58 +0600

    feat: complete 1 questions

--- 05.02.22/script.js
+++ 05.02.22/script.js
@@ -1,5 +1,11 @@
 //1 задания
-
+let players = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11]
+let i = 0;
+for (i; i < players.length; i++){
+    if (i > 0 && i < 10){
+        console.log(i);
+    }
+}
 
 //2 задания
 
