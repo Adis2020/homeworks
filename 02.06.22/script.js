32a8e3a600b988ace2805f1ed07ca67ad9d62824
adisk <adiskd22gm@gmail.com>
2022-06-09 14:57:07 +0600

    feat: complete 2 questions

---
+++ 02.06.22/script.js
@@ -0,0 +1,15 @@
+//1 задания
+let arr = ['a', 'b', 'c', 'd'];
+alert(`${arr[0]} + ${arr[1]},  ${arr[2]} + ${arr[3]}`);
+
+//2 задания
+arr = [10, 15, 9, 10]
+let Solution = arr[0] * arr[1];
+let Solution2 = arr[2] * arr[3];
+let result = (Solution + Solution2);
+alert(result);
+
+//3 Задания
+arr = ['a', 'b', 'c'];
+arr.push(1, 2 ,3);
+alert(arr);
