9b9fd8a0b77cbe49b3a3a36281bd667879710eeb
adisk <adiskd22gm@gmail.com>
2022-06-09 15:03:58 +0600

    feat: complete 1 questions fix: fixed 'title'

--- 07.06.22/script.js
+++ 07.06.22/script.js
@@ -1,21 +1,7 @@
-//1 задания
-console.log('1 задания');
-let players = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11]
-let i = 0;
-for (i; i < players.length; i++){
-    if (i > 0 && i < 10){
-        console.log(i);
-    }
+// 1 Задание
+function showMessage() {
+    alert('Привет, JavaScript!');
 }
-
-//2 задания
-console.log('2 задания');
-let arr = [1, 2, 5, 6, 88, 5]
-let sum = 0;
-for (i = 0; i < arr.length; i++){
-    sum += arr[i];
-}
-console.log(sum);
+showMessage();
 
 
-//3 Задания
