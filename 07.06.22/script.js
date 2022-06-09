394036aee7d89d2f781edd814df77bb3652c788e
adisk <adiskd22gm@gmail.com>
2022-06-09 15:06:00 +0600

    feat: complete 2 questions

--- 07.06.22/script.js
+++ 07.06.22/script.js
@@ -4,4 +4,9 @@ function showMessage() {
 }
 showMessage();
 
-
+// 2 Задание
+function additionsCube(number1, number2, number3) {
+    let result = number1 * number2 * number3;
+    alert(result);
+}
+additionsCube(5, 5, 5);
