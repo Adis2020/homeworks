16e29ba9f21b6c6dbd1d7ded7c5438232bbcfc05
adisk <adiskd22gm@gmail.com>
2022-06-11 21:41:09 +0600

    feat: 3 questions

---
+++ 11.06.22/script.js
@@ -0,0 +1,18 @@
+//1 Задание
+function calcNumber(a, b) {
+    return a - b;
+}
+let number = prompt('Введите первое число');
+let number2 = prompt('Введите второе число');
+let result = calcNumber(number, number2);
+alert(result);
+
+//2 Задание
+let message;
+function getMessage() {
+    message = prompt('Написать сообщение');
+    alert(message);
+}
+getMessage();
+
+//3 Задание
