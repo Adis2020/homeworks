321712b94dc02ccc6fadfba254dbde99389af3a6
adisk <adiskd22gm@gmail.com>
2022-06-15 16:07:34 +0600

    feat: added and completed 1 and 2 questions

---
+++ 14.06.22/script.js
@@ -0,0 +1,14 @@
+//1 Задание
+let arr = []
+for (let i = 0; i < 10; i++){
+    arr.push('x');
+}
+console.log(arr);
+
+//2 Задание
+arr = [1, 2, 115, 4, 58, 5 ,7 ,99]
+for (let i = 0; i < arr.length; i++){
+    if (arr[i] === 4){
+        console.log('ЕСТЬ!');
+    }
+}
