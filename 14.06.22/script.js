5e13e9115560b6d37e835aeee4ec66cc12367a99
adisk <adiskd22gm@gmail.com>
2022-06-16 18:40:52 +0600

    feat: added classWork

--- 14.06.22/script.js
+++ 14.06.22/script.js
@@ -1,5 +1,35 @@
 //1 Задание
 let arr = []
+function calcArr(text, number) {
+    for (let i = 0; i < number; i++){
+        arr[i] = text;
+    }
+    console.log(arr);
+}
+calcArr('JavaScript', 10);
+
+//2 Задание
+function calcResult(a, b){
+    if (a > 90 || b > 10){
+        console.log('Ваша результат 100');
+    }
+    else if (a >= 75 & b >= 5){
+        console.log('Ваш результат 90')
+    }
+    else if (a >= 50 & b >= 2){
+        console.log('Ваш результат 75')
+    }
+    else if (a >= 30 & b >= 1){
+        console.log('Ваш результат 50')
+    }
+    else console.log('Ваш результат 0')
+}
+calcResult(50, 2);
+
+
+//Д/З
+//1 Задание
+arr = []
 for (let i = 0; i < 10; i++){
     arr.push('x');
 }
