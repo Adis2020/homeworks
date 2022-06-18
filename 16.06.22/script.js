585a3e086163ef8492458de168e8e386f3ba4b8c
adisk <adiskd22gm@gmail.com>
2022-06-18 21:46:16 +0600

    feat: completed 1 question

--- 16.06.22/script.js
+++ 16.06.22/script.js
@@ -1,4 +1,10 @@
 //1 Задание
+let array = [2, 3, 4, 5, 6, 7];
+let result = 1;
+for (let i = 0; i < array.length; i++){
+    result *= array[i];
+}
+console.log(result);
 
 //2 Задание
 function myFunc(array) {
