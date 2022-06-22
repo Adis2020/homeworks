2adefbdfdb5c6be69db810422bb78a2023841db9
adisk <adiskd22gm@gmail.com>
2022-06-22 21:21:05 +0600

    fix: 2 questions

--- 21.06.22/script.js
+++ 21.06.22/script.js
@@ -1,9 +1,22 @@
+//Классная работа
+const numbers = [1, 4, 3, 23, 5, 45];
+const result = numbers.reduce((sum, item)=>{
+    return sum + item;
+}, 0)
+console.log(result);
+/*1) 0 + 1 = 1
+2) 1 + 4 = 5
+3) 5 + 3 = 23
+4) 23 + 5 = 28
+5) 28 + 45 = 78*/
+
+//Д/З
 //1 Задание
 function mul(n, m) {
     return n + m;
 }
-let result = mul(6, 8);
-console.log(result);
+let newResult = mul(6, 8);
+console.log(newResult);
 
 //2 Задание
 function factorial(n) {
@@ -15,5 +28,5 @@ function factorial(n) {
     return result;
 }
 
-result = factorial(7);
-console.log(result);
+newResult = factorial(7);
+console.log(newResult);
