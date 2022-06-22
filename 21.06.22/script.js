1b6eea95fd28e2b4ea8765c642a514d40e5d9e08
adisk <adiskd22gm@gmail.com>
2022-06-22 18:29:15 +0600

    feat: added 2 questions

--- 21.06.22/script.js
+++ 21.06.22/script.js
@@ -4,3 +4,14 @@ function mul(n, m) {
 }
 let result = mul(6, 8);
 console.log(result);
+
+function factorial(n) {
+    let result = 1;
+    for (let i = 0; i < n; i++){
+        result *= n--;
+    }
+    return result;
+}
+
+result = factorial(6);
+console.log(result);
