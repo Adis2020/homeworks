d7d8e2da6c3381ddbe44d59df501846da78ad752
adisk <adiskd22gm@gmail.com>
2022-07-05 16:09:47 +0600

    feat: more news

--- 02.07.22/script.js
+++ 02.07.22/script.js
@@ -1,9 +1,12 @@
-const clickImage1 = document.querySelector('#click_image1');
-const clickImage2 = document.querySelector('#click_image2');
-const clickImage3 = document.querySelector('#click_image3');
+const clickImage1 = document.querySelector('#headphones_click_img1');
+const clickImage2 = document.querySelector('#baloon_click_img2');
+const clickImage3 = document.querySelector('#backpack_click_img3');
 
-function getInfo() {
-    console.log(event.currentTarget)
+function getInfo(event) {
+    let id = event.target.id;
+    if (id === 'headphones_click_img1') alert('Это наушники. Красно-серые');
+    if (id === 'baloon_click_img2') alert('Это воздушный шар. Красный');
+    if (id === 'backpack_click_img3') alert('Это рюкзак. Красный');
 }
 
 clickImage1.addEventListener('click', getInfo);
