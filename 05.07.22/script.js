25281e88c02926ea66fa6c84d0be96c06386e953
adisk <adiskd22gm@gmail.com>
2022-07-07 19:05:57 +0600

    fix: fixed 2 questions

--- 05.07.22/script.js
+++ 05.07.22/script.js
@@ -9,13 +9,17 @@ clickNewColor.addEventListener('click', () => {
 const notification = document.querySelector('#show_notification');
 const button = document.querySelector('#click_button')
 
-function showNotification(){
+function showNotification(option){
     notification.classList.add('notification');
-    notification.textContent = 'Hello!';
+    notification.textContent = option.html;
     setTimeout(() => {
         notification.classList.remove('notification');
         notification.textContent = '';
     }, 1500);
 }
 
-button.addEventListener('click', showNotification);
+button.addEventListener('click', () => {
+    showNotification({
+        html: 'Hello',
+    })
+});
