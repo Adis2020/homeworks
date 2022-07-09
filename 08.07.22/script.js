77cd4648282446ef24699045db166652ae7e27c4
adisk <adiskd22gm@gmail.com>
2022-07-09 19:15:59 +0600

    fix: question

--- 08.07.22/script.js
+++ 08.07.22/script.js
@@ -1,64 +1,35 @@
 function showNotification(option) {
     const notification = document.querySelector('#show_notification');
     const wrapper = document.createElement('div');
-    if (option.className === 'alert-primary'){
-        wrapper.className = option.className;
-        wrapper.innerText = option.html;
-        wrapper.className = 'notification';
-        wrapper.style.backgroundColor = '#57b9ff';
-        notification.append(wrapper);
-    }else if (option.className === 'alert-secondary'){
-        wrapper.className = option.className;
-        wrapper.innerText = option.html;
-        wrapper.className = 'notification';
-        wrapper.style.backgroundColor = '#8f8f8f';
-        notification.append(wrapper);
-    }else if (option.className === 'alert-success'){
-        wrapper.className = option.className;
-        wrapper.innerText = option.html;
-        wrapper.className = 'notification';
-        wrapper.style.backgroundColor = '#40d66b';
-        notification.append(wrapper);
-    }else if (option.className === 'alert-danger'){
-        wrapper.className = option.className;
-        wrapper.innerText = option.html;
-        wrapper.className = 'notification';
-        wrapper.style.backgroundColor = '#d13636';
-        notification.append(wrapper);
-    }else if (option.className === 'alert-warning'){
-        wrapper.className = option.className;
-        wrapper.innerText = option.html;
-        wrapper.className = 'notification';
-        wrapper.style.backgroundColor = '#dbde50';
-        notification.append(wrapper);
-    }else if (option.className === 'alert-info'){
-        wrapper.className = option.className;
-        wrapper.innerText = option.html;
-        wrapper.className = 'notification';
-        wrapper.style.backgroundColor = '#50c4de';
-        notification.append(wrapper);
-    }else if (option.className === 'alert-light'){
-        wrapper.className = option.className;
-        wrapper.innerText = option.html;
-        wrapper.className = 'notification';
-        wrapper.style.backgroundColor = '#fff';
-        notification.append(wrapper);
-    }else if (option.className === 'alert-dark'){
-        wrapper.className = option.className;
-        wrapper.innerText = option.html;
-        wrapper.className = 'notification';
-        wrapper.style.backgroundColor = '#7d7d7d';
-        notification.append(wrapper);
-    }else {
-        console.log('Ошибка побробуйте снова');
-    }
+    wrapper.innerText = option.html;
+    wrapper.className = `notification ${option.className}`;
+    notification.append(wrapper);
     setTimeout(() => {
-        wrapper.classList.remove(option.className, 'notification');
-        wrapper.textContent = '';
+        wrapper.remove();
     }, 1500);
 }
 
-showNotification({
-    html: 'HELLO!',
-    className: 'alert-success'
+const buttonSuccess = document.querySelector('#button_success');
+const buttonWarning = document.querySelector('#button_warning');
+const buttonDanger = document.querySelector('#button_danger');
+
+buttonSuccess.addEventListener('click', () => {
+    showNotification({
+        html: 'Success!',
+        className: 'alert-success'
+    })
+});
+
+buttonWarning.addEventListener('click', () => {
+    showNotification({
+        html: 'Warning!',
+        className: 'alert-warning'
+    })
+});
+
+buttonDanger.addEventListener('click', () => {
+    showNotification({
+        html: 'Danger!',
+        className: 'alert-danger'
+    })
 });
