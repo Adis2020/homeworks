6ab90549221bef3f2e0e9b7d928644af36ac6199
adisk <adiskd22gm@gmail.com>
2022-07-10 21:54:17 +0600

    init: start

--- 08.07.22/script.js
+++ 08.07.22/script.js
@@ -1,8 +1,12 @@
 function showNotification(option) {
     const notification = document.querySelector('#show_notification');
     const wrapper = document.createElement('div');
+    const image = document.createElement('img');
+    image.src = option.image;
+
     wrapper.innerText = option.html;
     wrapper.className = `notification ${option.className}`;
+    wrapper.append(image);
     notification.append(wrapper);
     setTimeout(() => {
         wrapper.remove();
@@ -16,20 +20,23 @@ const buttonDanger = document.querySelector('#button_danger');
 buttonSuccess.addEventListener('click', () => {
     showNotification({
         html: 'Success!',
-        className: 'alert-success'
+        className: 'alert-success',
+        image: 'img/success.png'
     })
 });
 
 buttonWarning.addEventListener('click', () => {
     showNotification({
         html: 'Warning!',
-        className: 'alert-warning'
+        className: 'alert-warning',
+        image: 'img/warning.png'
     })
 });
 
 buttonDanger.addEventListener('click', () => {
     showNotification({
         html: 'Danger!',
-        className: 'alert-danger'
+        className: 'alert-danger',
+        image: 'img/danger.png'
     })
 });
