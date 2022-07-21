de05fa8eb0680ad53e482eb7895b478f99aef049
adisk <adiskd22gm@gmail.com>
2022-07-21 18:40:36 +0600

    fix: fixed name button to form

--- megastore/script.js
+++ megastore/script.js
@@ -3,9 +3,11 @@ const selector = document.querySelector('select');
 
 form.addEventListener('submit', () => {
     let inputs = document.querySelectorAll('input');
-    let array = ['surName', 'name', 'middleName', 'dateBirth', 'placeRegistration', 'ANID', 'tin', 'dateGet','bank','address'];
-    localStorage.bank = selector;
-    for (let i = 0; i < inputs.length; i++) localStorage.setItem(array[i], inputs[i].value);
+    let array = ['surName', 'name', 'middleName', 'dateBirth', 'placeRegistration', 'ANID', 'tin', 'dateGet', 'bank','address'];
+    for (let i = 0; i < inputs.length; i++) {
+        localStorage.setItem(array[i], inputs[i].value);
+    }
+    localStorage.bank = selector.value;
     if (localStorage['surName'] && localStorage['name'] && localStorage['middleName']) window.location.href = 'form2.html';
     console.log(localStorage)
 })
