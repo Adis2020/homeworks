134570c4f5b45cb51933edc5d41ad9d26fb0676c
adisk <adiskd22gm@gmail.com>
2022-07-27 14:45:27 +0600

    feat: added new function

--- megastore/script.js
+++ megastore/script.js
@@ -1,8 +1,9 @@
 const form = document.querySelector('#form');
 
-function createElement (name, textName, className){
-    const element = document.createElement(name);
-    element.innerText = textName;
+//Вызывая функцию писать все данные через кавычки
+function createElement (elementName, text, className){
+    const element = document.createElement(elementName);
+    element.innerText = text;
     element.className = className;
     return element;
 }
@@ -11,10 +12,9 @@ form.addEventListener('submit', () => {
     let inputs = document.querySelectorAll('#form input, #form select');
     for (let i = 0; i < inputs.length; i++) {
         if (inputs[i].hasAttribute('data-required')){
-            const p = document.createElement('p');
+            let p;
             if (inputs[i].value === '') {
-                p.innerText = 'Обязательное поле';
-                p.className = 'error-text';
+                p = createElement('p', 'Обязательное поле!', 'error-text');
                 inputs[i].before(p);
                 inputs[i].classList.add('red-square');
             }
