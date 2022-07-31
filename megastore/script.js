ac5e95f66d7de29559e8af7505c1e74cb22b2ed2
adisk <adiskd22gm@gmail.com>
2022-08-01 01:30:28 +0600

    feat: added 2 new function

--- megastore/script.js
+++ megastore/script.js
@@ -1,6 +1,5 @@
 const form = document.querySelector('#form');
 
-//Вызывая функцию писать все параметры через кавычки
 function createElement (elementName, text, className){
     const element = document.createElement(elementName);
     element.innerText = text;
@@ -8,20 +7,37 @@ function createElement (elementName, text, className){
     return element;
 }
 
+function checkElem (event) {
+    const element = document.querySelectorAll('.error-text');
+    let i;
+    for (i = 0; i < element.length; i++)
+    if (event.target.value !== ''){
+        event.target.classList.remove('red-square');
+        if (event.target.previousElementSibling === element[i]){
+            event.target.previousElementSibling.remove()
+        }
+    }
+}
+
+function findElem () {
+    const elements = document.querySelectorAll('.error-text');
+    for (let i = 0; i < elements.length; i++){
+        if (elements[i].className === 'error-text'){
+            elements[i].remove();
+        }
+    }
+}
+
 form.addEventListener('submit', () => {
     let inputs = document.querySelectorAll('#form input, #form select');
+    findElem();
     for (let i = 0; i < inputs.length; i++) {
         if (inputs[i].hasAttribute('data-required')){
-            let p;
             if (inputs[i].value === '') {
-                p = createElement('p', 'Обязательное поле!', 'error-text');
-                inputs[i].before(p);
                 inputs[i].classList.add('red-square');
+                inputs[i].before(createElement('p', 'Обязательное поле!', 'error-text'));
+                inputs[i].addEventListener('input', checkElem)
             }
-            inputs[i].addEventListener('change', () => {
-                inputs[i].classList.remove('red-square');
-                p.remove();
-            })
         }
     }
 })
