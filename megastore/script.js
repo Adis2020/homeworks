640308e8c41d12a8226e5e79376cca4af2a8648b
adisk <adiskd22gm@gmail.com>
2022-08-03 19:47:04 +0600

    feat: added new function 'makeObject' fix: fixed function on 'findElem'

--- megastore/script.js
+++ megastore/script.js
@@ -9,8 +9,7 @@ function createElement (elementName, text, className){
 
 function checkElem (event) {
     const element = document.querySelectorAll('.error-text');
-    let i;
-    for (i = 0; i < element.length; i++)
+    for (let i = 0; i < element.length; i++)
     if (event.target.value !== ''){
         event.target.classList.remove('red-square');
         if (event.target.previousElementSibling === element[i]){
@@ -21,23 +20,36 @@ function checkElem (event) {
 
 function findElem () {
     const elements = document.querySelectorAll('.error-text');
-    for (let i = 0; i < elements.length; i++){
-        if (elements[i].className === 'error-text'){
+    if (elements.length > 0){
+        for (let i = 0; i < elements.length; i++){
             elements[i].remove();
         }
     }
 }
 
+function makeObject (elements) {
+    const data = {};
+    for (let i = 0; i < elements.length; i++){
+        data[elements[i].id] = elements[i].value;
+    }
+
+}
+
 form.addEventListener('submit', () => {
-    let inputs = document.querySelectorAll('#form input, #form select');
+    const inputs = document.querySelectorAll('#form input, #form select');
+    let count = document.querySelectorAll('[data-required]').length;
     findElem();
     for (let i = 0; i < inputs.length; i++) {
         if (inputs[i].hasAttribute('data-required')){
             if (inputs[i].value === '') {
                 inputs[i].classList.add('red-square');
                 inputs[i].before(createElement('p', 'Обязательное поле!', 'error-text'));
-                inputs[i].addEventListener('input', checkElem)
+                inputs[i].addEventListener('input', checkElem);
+                count--;
             }
         }
+    }
+    if (count === document.querySelectorAll('[data-required]').length){
+        makeObject(inputs);
     }
 })
